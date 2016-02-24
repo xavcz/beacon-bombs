4b394b0587d3c3d8eb36e68a46a0d80bb972b059
xavizalote <xavier.cazalot@gmail.com>
2016-02-24 11:32:32 +0000

    test reactivebeaconregion

--- beacon-bombs.js
+++ beacon-bombs.js
@@ -1,23 +1,23 @@
+if (Meteor.isCordova) {
+  Meteor.startup(() =>  {
+    const reactiveBeaconRegion = new ReactiveBeaconRegion({identifier: "door beacon", uuid: "D0D3FA86-CA76-45EC-9BD9-6AF47CFFF8B8"});
+
+    console.log(reactiveBeaconRegion.getBeaconRegion());
+  });
+}
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    result () {
+      return reactiveBeaconRegion.getBeaconRegion();
     }
   });
 
   Template.hello.events({
-    'click button': function () {
+    'click button' () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
