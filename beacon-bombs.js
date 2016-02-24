a3ece22fbb87de87b7bc5c5a2ed0ba432f0977f8
xavizalote <xavier.cazalot@gmail.com>
2016-02-24 14:45:04 +0000

    test w/ lukas' code

--- beacon-bombs.js
+++ beacon-bombs.js
@@ -1,26 +1,25 @@
-if (Meteor.isClient) {
-  Template.hello.onCreated(function () {
-    let reactiveBeaconRegion;
-    if (Meteor.isCordova) {
-      reactiveBeaconRegion = new ReactiveBeaconRegion({
-        identifier: "door beacon",
-        uuid: "D0D3FA86-CA76-45EC-9BD9-6AF47CFFF8B8"
-      });
+lookingForBeacons = [
+  {
+    uuid: 'D0D3FA86-CA76-45EC-9BD9-6AF47CFFF8B8',
+    identifier: 'kevins'
+  }
+];
 
-      Tracker.autorun(() => {
-        console.log(reactiveBeaconRegion.getBeaconRegion());
+if (Meteor.isCordova) {
+  Meteor.startup(function() {
+    lookingForBeacons.forEach(function(myBeacon) {
+      var reactiveBeaconRegion = new ReactiveBeaconRegion({
+        identifier: myBeacon.identifier,
+        uuid: myBeacon.uuid
+      });
+      Tracker.autorun(function () {
         if (reactiveBeaconRegion.getBeaconRegion().inRegion) {
-          alert('yeah!')
+          var foundBeacons = reactiveBeaconRegion.getBeaconRegion().beacons;
+          foundBeacons.forEach(function(foundBeacon) {
+          });
+          console.log(foundBeacons);
         }
       });
-    }
-  });
-
-  Template.hello.events({
-    'click button' () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+    });
   });
 }
-
