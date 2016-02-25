b339f4c25fc24c9fc7608c523f01c1a611499ff8
Elvira Poiters <elvira.poiters@23seconds.be>
2016-02-25 15:29:58 +0000

    Mobile

--- mobile-config.js
+++ mobile-config.js
@@ -5,18 +5,18 @@ App.info({
   id: 'beacons.bombs',
   name: 'Beacon Bombs',
   description: 'Sequentially diffuse 4 bombs, in 10 mins, to save the Hostel!',
-  author: 'XavElvPatDan',
-});
+  author: 'XavElvPatDan'
+})
 
 // Set up resources such as icons and launch screens.
 App.icons({
   'iphone': 'icons/bomb.svg',
-  'iphone_2x': 'icons/bomb.svg',
+  'iphone_2x': 'icons/bomb.svg'
 });
 
 App.launchScreens({
   'iphone': 'splash/splash.jpg',
-  'iphone_2x': 'splash/splash.jpg',
+  'iphone_2x': 'splash/splash.jpg'
   // ... more screen sizes and platforms ...
 });
 
@@ -25,3 +25,5 @@ App.setPreference('BackgroundColor', '0xff808080');
 App.setPreference('HideKeyboardFormAccessoryBar', true);
 App.setPreference('Orientation', 'default');
 App.setPreference('Orientation', 'all', 'ios');
+
+//eanah72785
